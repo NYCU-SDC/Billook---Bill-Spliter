f9901395cfa4296903f7621119c12013b2e73a23
TwoFourTwo <johnny11118@gmail.com>
2024-03-23 13:50:36 +0800

    lesson 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './Components/Layout';
+import BooksList from './Components/BooksList';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<h1>Home</h1>} />
+                <Route path="mybook" element={<BooksList />} />
+                <Route path="joinbook" element={<h1>join-book</h1>} />
+                <Route path="login" element={<h1>login</h1>} />
+                <Route path="*" element={<h1>Error 404: Not Found</h1>} />
+            </Route>
+        </Routes>      
+        </BrowserRouter>
+    );
 }
 
 export default App;
